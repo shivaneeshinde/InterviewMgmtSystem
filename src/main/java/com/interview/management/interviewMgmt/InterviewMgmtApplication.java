78a3a9add110e5daa264454da42db9e3ab77d55a
Shivanee Deepak Shinde <shivanee.shinde@oracle.com>
2021-03-15 14:36:12 +0530

    first commit

---
+++ src/main/java/com/interview/management/interviewMgmt/InterviewMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.interview.management.interviewMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMgmtApplication.class, args);
+	}
+
+}
