ad7070dd6352e4a7e7cab4e8a84f39ad99c42184
Shivanee Deepak Shinde <shivanee.shinde@oracle.com>
2021-03-15 15:57:18 +0530

    index page code pushed

--- src/main/java/com/interview/management/interviewMgmt/controller/RestController.java
+++ src/main/java/com/interview/management/interviewMgmt/controller/RestController.java
@@ -3,29 +3,41 @@ package com.interview.management.interviewMgmt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.interview.management.interviewMgmt.entity.Details;
 import com.interview.management.interviewMgmt.service.DetailService;
 
-@org.springframework.web.bind.annotation.RestController
+//@org.springframework.web.bind.annotation.RestController
+@Controller
 public class RestController {
 	
 	
 	@Autowired
 	private DetailService detailService;
-	
+
+	@ResponseBody
 	@GetMapping("/healthCheck")
 	public String check()
 	{
 		return "checked";
 	}
 	
+	@ResponseBody
 	@GetMapping("/detailsList")
 	public Details detailsList() {
 	    System.out.println("Inside detailsList method");
 		List<Details> details = detailService.getDetailsList();
 		return details.get(0);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("message", "hey there");
+		return "index";
 	}
 
 }
